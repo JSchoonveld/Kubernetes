a1bfbbd1357cd18319dda1a144e77a1870c39001
JSchoonveld <jesse.schoonveld@gmail.com>
2021-10-13 14:55:56 +0200

    form

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,5 +1,9 @@
 <form action="">
     <input type="text">
-    <input type="text">
+    <select value="">
+        <option value="">Select a font</option>
+        <option value="">Arial</option>
+        <option value="">Times New Roman</option>
+    </select>
     <button>Submit</button>
 </form>
