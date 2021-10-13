ef19700d1936b6f963f7008a6c97917c4db5b9fa
JSchoonveld <jesse.schoonveld@gmail.com>
2021-10-13 15:05:57 +0200

    file watcher sass en begin form

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,9 +1,11 @@
 <form action="">
-    <input type="text">
-    <select value="">
-        <option value="">Select a font</option>
-        <option value="">Arial</option>
-        <option value="">Times New Roman</option>
-    </select>
-    <button>Submit</button>
+    <div class="form-container">
+        <input type="text">
+        <select value="">
+            <option value="">Select a font</option>
+            <option value="">Arial</option>
+            <option value="">Times New Roman</option>
+        </select>
+        <button>Submit</button>
+    </div>
 </form>
--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,12 +1,12 @@
 <x-layout>
     <x-slot name="content">
         <h1>Test</h1>
+        <x-form>
+
+        </x-form>
     </x-slot>
 
 </x-layout>
 
-<x-form>
 
-</x-form>
 
-<x-button>Submit</x-button>
