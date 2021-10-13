2a8ead102279b739032794a3c0976cdff48dce48
JSchoonveld <jesse.schoonveld@gmail.com>
2021-10-13 14:28:59 +0200

    webpack mix sass

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,5 +1,12 @@
 <form action="">
-    <input type="text">
-    <input type="text">
-    <button>Submit</button>
+    <div class="form-container">
+        <label for="text-body">Text</label>
+        <input id="text-body" name="text-body" type="text">
+        <select name="" id="">
+            <option value="">Select a font</option>
+            <option value="">Ariel</option>
+            <option value="">Times new roman</option>
+        </select>
+        <button>Submit</button>
+    </div>
 </form>
