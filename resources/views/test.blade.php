632d9f567300898413d77e3e6f7e2f2b4a83ab9c
JSchoonveld <jesse.schoonveld@gmail.com>
2021-10-13 14:28:59 +0200

    webpack mix sass

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -1,12 +1,13 @@
 <x-layout>
     <x-slot name="content">
-        <h1>Test</h1>
+        <h1>Form</h1>
+        <x-form>
+
+        </x-form>
     </x-slot>
 
 </x-layout>
 
-<x-form>
 
-</x-form>
 
-<x-button>Submit</x-button>
+
