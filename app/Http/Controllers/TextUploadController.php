06461b9b95936dfc0b42ee550ac0386959ec551f
JouriR <jouri.roosjen@home.nl>
2021-11-03 16:04:25 +0100

    Send SVG to container

--- app/Http/Controllers/TextUploadController.php
+++ app/Http/Controllers/TextUploadController.php
@@ -2,15 +2,14 @@
 
 namespace App\Http\Controllers;
 
-
+use Illuminate\Support\Facades\Http;
 use SVG\Nodes\Texts\SVGText;
 use SVG\SVG;
 use SVG\Nodes\Shapes\SVGRect;
 use Illuminate\Http\Request;
-
 class TextUploadController extends Controller
 {
- public function create()
+    public function create()
     {
     }
 
@@ -18,13 +17,13 @@ class TextUploadController extends Controller
     {
         dd($request->all());
     }
-    public function makeSvg(request $request)
+    public static function makeSvg(request $request)
     {
         $image = new SVG(400, 200);
         $doc = $image->getDocument();
         $font = new \SVG\Nodes\Structures\SVGFont('Arial', 'Helvetica, sans-serif');
 
-// blue 40x40 square at (0, 0)
+        // blue 40x40 square at (0, 0)
         $text = new SVGText($request['text-body'], 20, 50);
         $text->setFont($font);
         $text->setSize($request['text-size']);
@@ -33,6 +32,6 @@ class TextUploadController extends Controller
         $doc->addChild($text);
 
         header('Content-Type: image/svg+xml');
-        echo $image;
+        Http::post('svg2png/script.php', [$image]);
     }
 }
